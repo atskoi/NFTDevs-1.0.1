b9175ad91ccc599038236e2f92347b34f7588439
Bruno-Jackson <bruno.jackson.dev@gmail.com>
2022-09-15 20:00:26 +0900

    Contract completed and deployed!

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x2b78ad87255a66eB4B48575e3D33b1f332A003f3";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
